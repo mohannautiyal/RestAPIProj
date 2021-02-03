233237e08aacbe2221faf4852daefa579a078a29
mohannautiyal <mohannautiyal80@gmail.com>
2021-02-03 20:54:33 +0530

    Update WelcomeController.java

--- RestApplication/src/main/java/com/home/app/controllers/WelcomeController.java
+++ RestApplication/src/main/java/com/home/app/controllers/WelcomeController.java
@@ -11,7 +11,7 @@ public class WelcomeController {
 	@GetMapping(value="/")
 	public String welcomeEmp() {
 		
-		return "Welcome to Madan's Home Page . Sample App for CI/CD v2.";
+		return "Welcome to Madan's Home Page . Sample App for CI/CD v3.";
 	}
 
 }
